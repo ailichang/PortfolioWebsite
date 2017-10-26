dd55f35352abd6973404a79b40af381532f6f6c2
Emily Chang <emilyailichang@gmail.com>
2017-10-25 19:01:02 -0700

    upload images

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,13 @@
 var resumeUrl = 'EmilyChangResume.pdf';
 
 $(document).ready(function() {
+    //opens resume pdf in another tab
     $('#resume-button').click(function(){
         var win = window.open(resumeUrl,'_blank');
         win.focus();
     });
+
+    //the page jumps to #work-anchor location
     $('#work-button').click(function(){
         var loc = document.location.toString().split('#')[0];
         document.location = loc + "#work-anchor";
